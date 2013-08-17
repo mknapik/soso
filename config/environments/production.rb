fa73c51dbc72a9914f61da491c98a187e7d30a97
Michał Knapik <mknapik@student.agh.edu.pl>
2013-08-17 11:27:56 +0200

    remove canonical host name and change heroku app name (refs #374)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Soso::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {host: 'soso.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'soso3.herokuapp.com'}
 
   #GA.tracker = "UA-GoogleAnalytics-Code"
 end
