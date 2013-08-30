15fe2ce901c597fa320878ea1d55c8b42cf420b9
Michał Knapik <mknapik@student.agh.edu.pl>
2013-08-30 02:37:37 +0100

    add .active to navigation links (@30m)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
     "alert-#{alert_type}"
   end
 
+  def nav_link_to(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
