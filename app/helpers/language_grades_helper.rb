e10bba7b103fff1e98794df4a75fe60f667f8c50
Michal Knapik <mknapik@student.agh.edu.pl>
2013-09-12 11:44:03 +0100

    create helper for displaying flipswitch (refs #420 @20m)

--- app/helpers/language_grades_helper.rb
+++ app/helpers/language_grades_helper.rb
@@ -1,2 +1,13 @@
 module LanguageGradesHelper
+  def onoff_tag(name, value = '1', checked = false, options = {})
+    html_options = {type: 'checkbox', name: name, id: sanitize_to_id(name), value: value, class: 'onoffswitch-checkbox'}.update(options.stringify_keys)
+    html_options['checked'] = 'checked' if checked
+    content_tag :div, class: 'onoffswitch' do
+      tag(:input, html_options) +
+      content_tag(:label, '', class: 'onoffswitch-label', for: name) do
+        content_tag(:div, '', class: 'onoffswitch-inner') +
+        content_tag(:div, '', class: 'onoffswitch-switch')
+      end
+    end
+  end
 end
