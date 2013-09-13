8dbc8f0bd341c386ec53e4692a05b92c072590be
Michal Knapik <mknapik@student.agh.edu.pl>
2013-09-13 14:42:11 +0100

    add settings cacheing (refs #420 @30m)

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -30,26 +30,38 @@ class Setting < ActiveRecord::Base
   validates :value, :year, :committee,
             presence: true
 
+  @@cache = {stage: Hash.new, year: Hash.new}
+
   def self.stage(committee_id)
-    Stage.find(Setting.where(committee_id: committee_id, year: self.year(committee_id), name: :stage).first.value)
+    @@cache[:stage][committee_id] || (@@cache[:stage][committee_id] = Stage.find(Setting.find(committee_id: committee_id, year: self.year(committee_id), name: :stage).value))
   end
 
-  def self.stage=(committee_id, new_stage)
-    setting = Setting.where(committee_id: committee_id, year: self.year(committee_id), name: :stage)
+  def self.stage=(value)
+    committee_id, new_stage = value
+    setting = Setting.find(committee_id: committee_id, year: self.year(committee_id), name: :stage)
     setting.value = new_stage
     setting.save
+    @@cache[:stage] = Hash.new
   end
 
   def self.year(committee_id)
-    year = Setting.where(committee_id: committee_id, year: 0, name: :year).first
-    return Date.current.year if year.nil?
-    year.value
+    @@cache[:year][committee_id] || (@@cache[:year][committee_id] = begin
+      year = Setting.where(committee_id: committee_id, year: 0, name: :year).first
+      if year.nil?
+        Date.current.year
+      else
+        year.value
+      end
+    end)
   end
 
-  def self.year=(committee_id, new_year)
+  def self.year=(value)
+    #raise 'a' unless value.is_a?( Array )
+    committee_id, new_year = value
     setting = Setting.where(committee_id: committee_id, year: 0, name: :year).first_or_initialize
     setting.value = new_year
     setting.save
+    @@cache[:year] = Hash.new
   end
 
 end
